9f9c90cb55b0c32baacf5de446ef5ea80551b035
Manuel <a20185361@pucp.edu.pe>
2022-11-08 21:34:26 -0500

    Diego Zuasnabar

---
+++ src/main/java/com/example/lab8/Daos/Ejercicio2Dao.java
@@ -0,0 +1,21 @@
+package com.example.lab8.Daos;
+
+import com.example.lab8.Entities.Coin;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class Ejercicio2Dao {
+    public List<Coin> listarCoin(int n){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Coin[]> response = restTemplate.getForEntity(
+                "https://api.coinstats.app/public/v1/coins?limit=70", Coin[].class);
+
+        return Arrays.asList(response.getBody());
+
+    }
+}
